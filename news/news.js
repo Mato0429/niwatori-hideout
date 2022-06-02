31a838f5171fe84f309cbe07a57aece4807755e0
Mato0429 <91228634+Mato0429@users.noreply.github.com>
2022-06-02 20:51:39 +0900

    Add files via upload

---
+++ news/news.js
@@ -0,0 +1,10 @@
+
+/*
+@Copyright - 2022 PepepeFactory
+I know no one will use this program even 
+if I don't write it, but it's interesting, so I'll write it.
+*/
+
+document.addEventListener("DOMContentLoaded",x =>{
+  
+});
